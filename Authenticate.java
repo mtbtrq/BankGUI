f1cc4e7a83f44892ca9bbff6bcd2eb7050aba80f
Supelion <mutayyabtariq9@gmail.com>
2021-11-03 22:15:05 +0300

    .

--- Authenticate.java
+++ Authenticate.java
@@ -3,30 +3,38 @@ import java.util.Objects;
 
 public class Authenticate {
 
+    // Class constructor
     public Authenticate (String userName, String pw) {
 
+        // All the global variables
         int tryCount = 0;
         int maxTryLimit = 10;
         String usersPassword = null;
         boolean isWrongPassword = false;
 
+        // While the user's inputted password (in the GUI) is not equal to the correct password, do this
         while (!Objects.equals(usersPassword, pw)) {
+            // If the user has entered the not entered a password incorrectly, do this
             if (!isWrongPassword) {
                 usersPassword = JOptionPane.showInputDialog(null, "Please enter your password:");
                 if (!Objects.equals(usersPassword, pw)) {
+                    // If the user does'nt enter the correct password on the first attempt, make isWrongPassword true
                     isWrongPassword = true;
                 }
             } else {
                 if (tryCount == maxTryLimit) {
+                    // If the user has had too many tries, do this
                     JOptionPane.showMessageDialog(null, "You've had too many incorrect tries!");
                     System.exit(0);
                 } else {
+                    // If the user is still in limit of the max tries he can perform, do this
                     tryCount += 1;
                     usersPassword = JOptionPane.showInputDialog(null, "Incorrect password entered!\n\nTries remaining: " + (maxTryLimit - tryCount) + "\n\nPlease enter your password:");
                 }
             }
         }
-        
+
+        // Once the user enters the correct password, perform the following        
         User user = new User(userName);
         user.showMenu();
     }
--- User.java
+++ User.java
@@ -1,6 +1,8 @@
 import javax.swing.JOptionPane;
 
 public class User {
+
+	// All global variables
     float balance;
 	char previousAction;
 	String customerName;
@@ -8,74 +10,105 @@ public class User {
 	Double randomNumber = Math.floor(Math.random()*(99999999+1)+0);
     int customerId = randomNumber.intValue();
 
+	// Class constructor
     public User (String customersName) {
         customerName = customersName;
     }
 
+	// Deposite method
     void deposit() {
+		// Asking user for input then converting that input from string to float
 		String stringAmount = JOptionPane.showInputDialog(null, "Enter an amount to deposit:");
 		float amount = Float.parseFloat(stringAmount);
 		
+		// If the user's inputted amount is not zero, perform the following
 		if (amount != 0) {
+			// Add amount to balance, set previousAction to "d" which stands for deposit, 
+			// and set previousActionAmount to the user's inputted amount
 			balance += amount;
 			previousAction = 'd';
 			previousActionAmount = amount;
 
+			// Show the user a success message
 			JOptionPane.showMessageDialog(null, "Deposited $" + amount + " to your bank account!");
 		}
     }
 
+	// Withdraw method
     void withdraw() {
+		// Asking user for input then converting that input from string to float
 		String stringAmount = JOptionPane.showInputDialog(null, "Enter an amount to withdraw:");
 		float amount = Float.parseFloat(stringAmount);
 
+		// If the user's inputted amount is not zero, perform the following
         if (amount != 0) {
+			// remove amount from balance, set previousAction to "w" which stands for withdraw, 
+			// and set previousActionAmount to the user's inputted amount
+			balance -= amount;
 			previousAction = 'w';
 			previousActionAmount = amount;
-			balance -= amount;
 
+			// Show the user a success message
 			JOptionPane.showMessageDialog(null, "Withdrew $" + amount + " from your bank account!");
         }
     }
 
+	// Previous transaction method
     void getPreviousTransaction() {
-        if (previousAction == 'd') {
-			JOptionPane.showMessageDialog(null, "Deposited $" + previousActionAmount);
-        } else if (previousAction == 'w') {
-			JOptionPane.showMessageDialog(null, "Withdrew $" + previousActionAmount);
-        } else {
-			JOptionPane.showMessageDialog(null, "No previous transaction occured.");
-        }
+		switch(previousAction) {
+			case 'd':
+				// If the previousAction is deposit, show the user the amount and the action
+				JOptionPane.showMessageDialog(null, "Deposited $" + previousActionAmount);
+				break;
+			case 'w':
+			// If the previousAction is withdraw, show the user the amount and the action
+				JOptionPane.showMessageDialog(null, "Withdrew $" + previousActionAmount);
+				break;
+			default:
+				// If there is no previous activity, show the user an error message
+				JOptionPane.showMessageDialog(null, "No previous transaction occured.");
+				break;
+		}
     }
 
+	// Main method which calls all of our functions
     void showMenu() {
 		char option = '\0';
 
 		do {
+			// A very long showInputDialog call
 			char option1 = JOptionPane.showInputDialog(null, "Welcome, " + customerName + "(" + customerId + ")" + "!" + "\n\n" + "What would you like to do?" + "\n" + "A. Check your balance" + "\n" + "B. Make a deposit" + "\n" + "C. Make a withdrawal" + "\n" + "D. View previous transaction" + "\n" + "E. Exit" + "\n\n" + "Enter an option:").charAt(0);
+			
+			// Making the user's selection uppercase to allow the user to type an option of any capitalization
 			option = Character.toUpperCase(option1);
 			
 			switch(option) {
 			case 'A':
+				// If the user wants to see their balance, do this
 				JOptionPane.showMessageDialog(null, "Balance = $" + balance);
 				break;
 			case 'B':
+				// If the users want to deposit a certain amount, call the deposit function
 				deposit();
 				break;
 			case 'C':
+			// If the users want to withdraw a certain amount, call the withdraw function
 				withdraw();
 				break;
 			case 'D':
+				// If the user wants to see their previous action, call the getPreviousTransaction function
 				getPreviousTransaction();
 				break;
 			case 'E':
 				break;
 			default:
+				// If the user is stupid and enters an invalid option, display an error
 				JOptionPane.showMessageDialog(null, "Error: invalid option. Please enter only enter A, B, C, D, or E!");
 				break;
 			}
 		} while (option != 'E');
 
+		// When the user exits the application, display a message
 		JOptionPane.showMessageDialog(null, "Thank you for banking with us!");
 	}
 }
